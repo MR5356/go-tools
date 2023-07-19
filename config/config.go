794a52cae0840509aa81b82a00db567946be83e3
Rui Ma <535657150@qq.com>
2023-07-19 17:16:04 +0800

    support watch consul config

--- config/config.go
+++
@@ -1,8 +0,0 @@
-package config
-
-type Config interface {
-	Get(key string) interface{}
-	GetOrDefault(key string, defaultValue any) any
-	Watch(key string, callback func())
-	UnWatch(key string)
-}
--- config/consul.go
+++ config/consul.go
@@ -3,6 +3,7 @@ package config
 import (
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
+	_ "github.com/spf13/viper/remote"
 	"sync"
 	"time"
 )
@@ -21,7 +22,18 @@ type ConsulConfig struct {
 	watcher map[string]func()
 }
 
-func NewConsulConfig(addr string, watchFrequency time.Duration) *ConsulConfig {
+func InitConsulConfig(addr, path, consulType string, watchFrequency time.Duration) {
+	err := viper.AddRemoteProvider("consul", addr, path)
+	if err != nil {
+		logrus.Fatalf("viper.AddRemoteProvider error: %v", err)
+	}
+
+	viper.SetConfigType(consulType)
+	err = viper.ReadRemoteConfig()
+	if err != nil {
+		logrus.Fatalf("viper.ReadRemoteConfig error: %v", err)
+	}
+
 	once.Do(func() {
 		config = &ConsulConfig{
 			Addr:           addr,
@@ -38,45 +50,43 @@ func NewConsulConfig(addr string, watchFrequency time.Duration) *ConsulConfig {
 				if err != nil {
 					logrus.Errorf("watch remote config err: %+v", err)
 				}
-				config.doWatch()
+				doWatch()
 			}
 		}()
 	})
-
-	return config
 }
 
-func (c *ConsulConfig) Get(key string) interface{} {
-	return viper.Get(key)
+func Get[T any](key string) T {
+	return viper.Get(key).(T)
 }
 
-func (c *ConsulConfig) GetOrDefault(key string, defaultValue any) any {
+func GetOrDefault[T any](key string, defaultValue T) T {
 	viper.SetDefault(key, defaultValue)
-	return viper.Get(key)
+	return viper.Get(key).(T)
 }
 
-func (c *ConsulConfig) Watch(key string, callback func()) {
-	c.lock.Lock()
-	defer c.lock.Unlock()
-	c.config[key] = viper.Get(key)
-	c.watcher[key] = callback
+func Watch(key string, callback func()) {
+	config.lock.Lock()
+	defer config.lock.Unlock()
+	config.config[key] = viper.Get(key)
+	config.watcher[key] = callback
 }
 
-func (c *ConsulConfig) UnWatch(key string) {
-	c.lock.Lock()
-	defer c.lock.Unlock()
-	delete(c.watcher, key)
-	delete(c.config, key)
+func UnWatch(key string) {
+	config.lock.Lock()
+	defer config.lock.Unlock()
+	delete(config.watcher, key)
+	delete(config.config, key)
 }
 
-func (c *ConsulConfig) doWatch() {
-	c.lock.Lock()
-	defer c.lock.Unlock()
-	for key, callback := range c.watcher {
-		ov := c.config[key]
+func doWatch() {
+	config.lock.Lock()
+	defer config.lock.Unlock()
+	for key, callback := range config.watcher {
+		ov := config.config[key]
 		nv := viper.Get(key)
 		if ov != nv {
-			c.config[key] = nv
+			config.config[key] = nv
 			callback()
 		}
 	}
